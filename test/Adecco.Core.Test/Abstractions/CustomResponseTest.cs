bce7357e88909eb28917625bc534cfe10ec3dbec
Rondinele Guimararães <rondinele@cia3r.com.br>
2024-03-22 13:20:01 -0300

    feat: Adicionando testes para CustomResponse

---
+++ test/Adecco.Core.Test/Abstractions/CustomResponseTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Adecco.Core.Test.Abstractions;
+public class CustomResponseTest
+{
+    [Fact]
+    public void CustomResponse_Deve_Inicializar_Com_Sucesso_True()
+    {
+        var response = new CustomResponse();
+        response.Success.Should().BeTrue();
+        response.GeneralErrors.Should().BeEmpty();
+        response.EntityErrors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void AddError_Deve_Adicionar_Erro_Geral_E_Alterar_Sucesso_Para_False()
+    {
+        var response = new CustomResponse();
+        response.AddError("Erro geral");
+
+        response.Success.Should().BeFalse();
+        response.GeneralErrors.Should().ContainSingle().And.Contain("Erro geral");
+    }
+
+    [Fact]
+    public void AddEntityError_Deve_Adicionar_Erro_De_Entidade_E_Alterar_Sucesso_Para_False()
+    {
+        var response = new CustomResponse();
+        response.AddEntityError("Entidade1", "Erro de entidade");
+
+        response.Success.Should().BeFalse();
+        response.EntityErrors.Should().HaveCount(1);
+        response.EntityErrors["Entidade1"].Should().ContainSingle().And.Contain("Erro de entidade");
+    }
+
+    [Fact]
+    public void ToString_Deve_Retornar_Todos_Os_Erros_Formatados()
+    {
+        var response = new CustomResponse();
+        response.AddError("Erro geral 1");
+        response.AddEntityError("Entidade1", "Erro de entidade 1");
+        response.AddEntityError("Entidade2", "Erro de entidade 2");
+
+        var expectedOutput = "Erro geral 1; Entidade1: Erro de entidade 1; Entidade2: Erro de entidade 2";
+        response.ToString().Should().Be(expectedOutput);
+    }
+
+    [Fact]
+    public void ToString_Deve_Retornar_String_Vazia_Se_Nao_Houver_Erros()
+    {
+        var response = new CustomResponse();
+        response.ToString().Should().BeEmpty();
+    }
+}
--- test/Adecco.Core.Test/Abstractions/CustomValidationResultTest.cs
+++ test/Adecco.Core.Test/Abstractions/CustomValidationResultTest.cs
@@ -2,18 +2,18 @@ namespace Adecco.Core.Test.Abstractions;
 
 public class CustomValidationResultTest
 {
-    private readonly CustomValidationResult validationResult;
+    private readonly CustomValidationResult _validationResult;
 
     public CustomValidationResultTest()
     {
-        validationResult = new CustomValidationResult();
+        _validationResult = new CustomValidationResult();
     }
 
     [Fact]
     public void Deve_Inicializar_Corretamente()
     {
-        validationResult.Errors.Should().BeEmpty();
-        validationResult.IsValid.Should().BeTrue();
+        _validationResult.Errors.Should().BeEmpty();
+        _validationResult.IsValid.Should().BeTrue();
     }
 
     [Theory]
@@ -21,9 +21,9 @@ public class CustomValidationResultTest
     [InlineData("Erro de validação", "CampoTeste", "CampoTeste: Erro de validação")]
     public void AdicionarErro_Deve_Funcionar_Corretamente(string errorMessage, string fieldName, string expectedError)
     {
-        validationResult.AddError(errorMessage, fieldName);
-        validationResult.Errors.Should().ContainSingle().And.Contain(expectedError);
-        validationResult.IsValid.Should().BeFalse();
+        _validationResult.AddError(errorMessage, fieldName);
+        _validationResult.Errors.Should().ContainSingle().And.Contain(expectedError);
+        _validationResult.IsValid.Should().BeFalse();
     }
 
     [Theory]
@@ -31,18 +31,18 @@ public class CustomValidationResultTest
     [InlineData(true, "Erro condicional", "CampoCondicional")]
     public void AdicionarErroSe_Deve_Funcionar_Corretamente(bool condition, string errorMessage, string fieldName)
     {
-        validationResult.AddErrorIf(condition, errorMessage, fieldName);
+        _validationResult.AddErrorIf(condition, errorMessage, fieldName);
         var expectedError = string.IsNullOrWhiteSpace(fieldName) ? errorMessage : $"{fieldName}: {errorMessage}";
-        validationResult.Errors.Should().ContainSingle().And.Contain(expectedError);
-        validationResult.IsValid.Should().BeFalse();
+        _validationResult.Errors.Should().ContainSingle().And.Contain(expectedError);
+        _validationResult.IsValid.Should().BeFalse();
     }
 
     [Fact]
     public void AdicionarErro_ComMensagemVazia_Deve_IgnorarErro()
     {
-        validationResult.AddError("");
-        validationResult.Errors.Should().BeEmpty();
-        validationResult.IsValid.Should().BeTrue();
+        _validationResult.AddError("");
+        _validationResult.Errors.Should().BeEmpty();
+        _validationResult.IsValid.Should().BeTrue();
     }
 
     [Fact]
@@ -58,11 +58,11 @@ public class CustomValidationResultTest
     [Fact]
     public void Erros_Deve_Ser_Imutavel()
     {
-        validationResult.AddError("Erro inicial");
-        var errorsBeforeModification = validationResult.Errors.ToList();       
-        validationResult.AddError("Erro adicional");     
+        _validationResult.AddError("Erro inicial");
+        var errorsBeforeModification = _validationResult.Errors.ToList();       
+        _validationResult.AddError("Erro adicional");     
         errorsBeforeModification.Should().HaveCount(1).And.Contain("Erro inicial");
-        validationResult.Errors.Should().HaveCount(2).And.Contain(new[] { "Erro inicial", "Erro adicional" });
+        _validationResult.Errors.Should().HaveCount(2).And.Contain(new[] { "Erro inicial", "Erro adicional" });
     }
 
 }
